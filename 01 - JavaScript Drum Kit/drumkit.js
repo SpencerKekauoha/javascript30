9b6454b3d897918b7c8ac8d7c77fd0a9b0116ae0
SpencerKekauoha <spkekauoha@gmail.com>
2016-12-11 17:03:29 -0700

    drumkit keydown events are firing

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,5 +1,4 @@
-
-
+console.log('hi');
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
